e37cd16a3c0e88aa1fe0a42bb6e49d7960168c76
Harish Kumar Gangula <harish@sanketika.in>
2021-07-19 18:42:55 +0530

    Implemented setContent and clearPrefs methods

--- src/ios/SplashScreen.swift
+++ src/ios/SplashScreen.swift
@@ -1,8 +1,41 @@
-    
+//
+//  Splashscreen.swift
+//  Sunbird
+//
+//  Created by Harish Kumar Gangula on 19/07/21.
+//
+
+
 import Foundation
 
 
-@objc(SplashScreen) class SplashScreen : CDVPlugin { 
+@objc(SplashScreen) class SplashScreen : CDVPlugin {
+
+    private let preferencesPrefix = "splashscreen"
+    private let KEY_LOGO = "app_logo";
+    private let KEY_NAME = "app_name";
+    private let KEY_IS_FIRST_TIME = "is_first_time";
+    
+    private let DEFAULT_SPLASHSCREEN_DURATION = 3000;
+    private let DEFAULT_FADE_DURATION = 500;
+
+private let preferences = UserDefaults.standard
+
+
+    fileprivate func setPreferences(key: String, value: Any) {
+        preferences.setValue(value, forKey: preferencesPrefix + "-" + key)
+    }
+    
+    fileprivate func getPreferences<T>(key: String, defaultValue: T) -> T {
+        return preferences.object(forKey: preferencesPrefix + "-" + key) == nil ? defaultValue : preferences.object(forKey: preferencesPrefix + "-" + key) as! T
+    }
+    
+    fileprivate func clearPreferences() {
+        preferences.removeObject(forKey: preferencesPrefix + "-" + KEY_LOGO)
+        preferences.removeObject(forKey: preferencesPrefix + "-" + KEY_NAME)
+        preferences.removeObject(forKey: preferencesPrefix + "-" + KEY_IS_FIRST_TIME)
+        
+    }
 
 
     @objc
@@ -23,10 +56,12 @@ import Foundation
 
     @objc
     func setContent(_ command: CDVInvokedUrlCommand) {
-        //TODO: set this in splash screen
         var pluginResult: CDVPluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
-        let appName = command.arguments[0] as? String 
-        let logoUrl = command.arguments[1] as? String 
+        let appName = command.arguments[0] as! String
+        let logoUrl = command.arguments[1] as! String
+        setPreferences(key: KEY_NAME, value: appName)
+        setPreferences(key: KEY_LOGO, value: logoUrl)
+        // TODO: Need to download logoUrl and set it to view using screeen dimensions
         pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: "")
         self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
         
@@ -38,6 +73,13 @@ import Foundation
         pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK)
         self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
         
+    }
+
+    @objc
+    func clearPrefs(_ command: CDVInvokedUrlCommand) {
+        clearPreferences()
+        let pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK)
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
     }
 
     @objc
